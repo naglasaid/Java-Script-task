92e9ccbda731907733b0f889e4e9494ddf41847e
Naglaa Said <146718126+naglasaid@users.noreply.github.com>
2023-11-07 13:37:27 +0200

    Add files via upload

---
+++ js/MyScript.js
@@ -0,0 +1,24 @@
+// var x =prompt("Enter Your Age:")
+// document.getElementById("div1").innerHTML=x
+
+
+
+
+// confirm("sdkskjdksjfks")
+//document.write("hello")
+// document.getElementById("div1").inner
+// document.getElementsByClassName("div1style").item(1).innerHTML="kfsjkdsjfkljdkf"
+// var showpricebtn= document.getElementById("show-price")
+// var div1=document.getElementById("div1")
+
+
+showpricebtn.onclick=function(){
+    div1.innerHTML="Naglaa"
+}
+
+// ////////////////////////////////////variables Declartions////////////////////////////////////
+
+var div1= document.querySelector("#div1")
+var showpricebtn = document.querySelector("show-price")
+var allproducts = document.querySelectorAll(".products li")
+var totalPrice=0
